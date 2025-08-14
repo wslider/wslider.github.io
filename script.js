d8540c2b57a661f710731a56a22d56a7e10bbe38
wslider <wslider2000@gmail.com>
2025-08-14 11:05:49 -0400

    soft reset

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
-function updateCssTheme (){
+function updateCssTheme() {
     const now = new Date();
     const hour = now.getHours();
-    linkElement = document.getElementById('customStyleSheet'); 
+    linkElement = document.getElementById('customStyleSheet');
     if (hour >= 6 && hour <= 14) {
-        linkElement.href="daytime.css"; 
+        linkElement.href = "daytime.css";
     }
-    else if (hour > 14 && hour <= 19){
-        linkElement.href="evening.css";
+    else if (hour > 14 && hour <= 19) {
+        linkElement.href = "evening.css";
     }
     else {
-        linkElement.href="night.css"
+        linkElement.href = "night.css";
     }
 }
 
-updateCssTheme (); 
-setInterval (updateCssTheme, 60000);
+updateCssTheme();
+setInterval(updateCssTheme, 60000);
 
 function navBarLinks() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
 
-
-// update image function or combine with update css 
-
-function updateImage () {
+function updateImage() {
     const now = new Date();
-    const hour = now.getHours(); 
+    const hour = now.getHours();
     customImage = document.getElementById('mainImage');
-        if (hour >= 6 && hour <= 14) {
-        customImage.src = "hurricaneRidge.jpg"; 
+    if (hour >= 6 && hour <= 14) {
+        customImage.src = "hurricaneRidge.jpg";
     }
-    else if (hour > 14 && hour <= 19){
-       customImage.src="tartooshSunset.jpg";
+    else if (hour > 14 && hour <= 19) {
+        customImage.src = "tartooshSunset.jpg";
     }
     else {
-        customImage.src="rainierNightSky.jpeg";
+        customImage.src = "rainierNightSky.jpeg";
     }
 }
 
 updateImage();
-setInterval (updateImage, 60000);
+setInterval(updateImage, 60000);
+
