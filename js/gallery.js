ec763a202d710641ebaa89660c360bed5bebc8a1
wslider <wslider2000@gmail.com>
2025-12-23 13:35:14 -0500

    updated html, css, js for gallery page + created gallery.json for future use

--- js/gallery.js
+++ js/gallery.js
@@ -1,30 +1,27 @@
-
-//click event to display new random image
-    // image array - match file names of images
-        //future: object array (change alt and caption of image)
-
-    // random number generator to select image from array index
-    // display image src = ` image array index `+ ".jpg"`
+import { navBarLinks } from "./utils.js";
+import { updateFooter } from "./utils.js";
 
 // Select DOM elements
-
 const galleryImage = document.getElementById('galleryImage');
 const imageButton = document.getElementById('imageButton');
 const imageCaption = document.getElementById('imageCaption');
 const searchImagesButton = document.getElementById('searchImagesButton');
 const searchImagesInput = document.getElementById('searchImagesInput');
+const customStyleSheet = document.getElementById('customStyleSheet');
 
 
-            // create images.JSON later 
+
+//fetch from images.JSON later
+
 const imageArray = [
-  { src: 'media/lushWaterfall.jpeg', alt: 'Lush Waterfall', caption: 'A serene waterfall in the forest' },
-  { src: 'media/jennyFallLeaves.jpg', alt: 'Jenny Fall Leaves', caption: 'Autumn leaves and Jenny' },
-  { src: 'media/hurricaneRidge.jpg', alt: 'Hurricane Ridge', caption: 'Snowy peaks at Hurricane Ridge, Olympic NP' },
-  { src: 'media/tartooshSunset.jpg', alt: 'Tartoosh Sunset', caption: 'Sunset over Tartoosh Mountains' },
-  { src: 'media/rainierNightSky.jpeg', alt: 'Night Sky and Mount Rainier', caption: 'Mount Rainier and Night Sky' },
-  { src: 'media/eclipse-20240408.jpeg', alt: 'Image of 2025 Solar Eclipse in Totality', caption: '2024 April Solar Eclipse' },
-  { src: 'media/smoky-mountain-sunset.jpg', alt: 'Image of the Sunset from Clingmans Dome', caption: 'Smoky Mountain Sunset' },
-  { src: 'media/swiftcurrentlake-sunrise-mountain.jpeg', alt: 'Image of Swiftcurrent Lake in Glacier National Park', caption: 'Swiftcurrent Lake, Glacier NP'}
+  { src: 'media/lushWaterfall.jpeg', alt: 'Lush Waterfall', caption: 'A serene waterfall in the forest' , cssTheme:'css/daytime.css'},
+  { src: 'media/jennyFallLeaves.jpg', alt: 'Jenny Fall Leaves', caption: 'Autumn leaves and Jenny' , cssTheme:'css/night.css'},
+  { src: 'media/hurricaneRidge.jpg', alt: 'Hurricane Ridge', caption: 'Snowy peaks at Hurricane Ridge, Olympic NP' , cssTheme:'css/evening.css'},
+  { src: 'media/tartooshSunset.jpg', alt: 'Tartoosh Sunset', caption: 'Sunset over Tartoosh Mountains' , cssTheme:'css/evening.css'},
+  { src: 'media/rainierNightSky.jpeg', alt: 'Night Sky and Mount Rainier', caption: 'Mount Rainier and Night Sky' , cssTheme:'css/night.css'},
+  { src: 'media/eclipse-20240408.jpeg', alt: 'Image of 2025 Solar Eclipse in Totality', caption: '2024 April Solar Eclipse' , cssTheme:'css/night.css'},
+  { src: 'media/smoky-mountain-sunset.jpg', alt: 'Image of the Sunset from Clingmans Dome', caption: 'Smoky Mountain Sunset' , cssTheme:'css/evening.css'},
+  { src: 'media/swiftcurrentlake-sunrise-mountain.jpeg', alt: 'Image of Swiftcurrent Lake in Glacier National Park', caption: 'Swiftcurrent Lake, Glacier NP', cssTheme:'css/night.css'}
 ];
 
 // Function to set a random image, alt text, and caption
@@ -41,6 +38,7 @@ function randomImage() {
     galleryImage.alt = selectedImage.alt;
     imageCaption.textContent = selectedImage.caption;
     galleryImage.style.opacity = 1;
+    customStyleSheet.href = selectedImage.cssTheme;
   }, 30);
 }
 
@@ -65,42 +63,50 @@ function searchImages(e) {
       galleryImage.src = searchResultImage.src;
       galleryImage.alt = searchResultImage.alt;
       imageCaption.textContent = searchResultImage.caption;
+      customStyleSheet.href = searchResultImage.cssTheme;
     } else {
       imageCaption.textContent = `No image found for "${searchTerm}"`;
       galleryImage.src = ''; // Clear the image or set a default
       galleryImage.alt = '';
+      customStyleSheet.href = 'css/evening.css';
     }
     galleryImage.style.opacity = 1;
   }, 30);
 }
 
-// Function to toggle navigation links
-function navBarLinks() {
-    const x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
 
-// Time of Day CSS Theme switching function
-function updateCssTheme() {
-  const now = new Date();
-  const hour = now.getHours();
-  const linkElement = document.getElementById('customStyleSheet');
-  if (hour >= 6 && hour <= 14) {
-    linkElement.href = 'css/daytime.css';
-  } else if (hour > 14 && hour <= 19) {
-    linkElement.href = 'css/evening.css';
-  } else {
-    linkElement.href = 'css/night.css';
-  }
-}
-
-updateCssTheme();
-setInterval(updateCssTheme, 60000);
 
 // Event listeners
 imageButton.addEventListener('click', randomImage);
+
 searchImagesButton.addEventListener('click', searchImages);
+
+document.getElementById('dropMenu').addEventListener('click', navBarLinks); 
+
+// Run when the DOM is ready
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', () => {
+
+        updateFooter();
+        setInterval(updateFooter, 3600000); // 1 hour 
+    });
+} else {
+
+        updateFooter();
+        setInterval(updateFooter, 3600000);
+}
+
+
+/*async function loadImageArray() {
+  try {
+    const response = await fetch('js/gallery.json');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const imageArray = await response.json();
+    return imageArray;
+  } catch (error) {
+    console.error('Error fetching gallery data:', error);
+    return [];
+  }
+}*/
