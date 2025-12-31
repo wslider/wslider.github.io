8ff2d0be6e538426d2b11705692d15e3e5077eb1
wslider <wslider2000@gmail.com>
2025-12-31 12:49:05 -0500

    fixed navbar in resume page

--- js/resume.js
+++ js/resume.js
@@ -1,14 +1,11 @@
-import { navBarLinks } from 'js/utils.js';
-import { updateFooter } from 'js/script.js';
-import { getUserRepos } from 'js/projects.js';
+import { navBarLinks } from '/js/utils.js';
+import { updateFooter } from '/js/utils.js';
 
-const container = document.getElementById('myProjects'); 
+
+
 
 document.getElementById('dropMenu').addEventListener('click', navBarLinks); 
-
-
-getUserRepos('wslider');
-setInterval(() => getUserRepos('wslider'), 3600000);
+navBarLinks();
 
 
 // Run when the DOM is ready
